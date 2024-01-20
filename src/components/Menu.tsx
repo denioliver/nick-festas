d861333f6a1727607d1febcaa1e86c6e4066c80c
Denivan Oliveira <denyoliver777@gmail.com>
2024-01-20 20:38:04 -0300

    feat: menu/burguer

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import '../styles/Menu.css';
 import { Link } from 'react-router-dom';
 import { listProducts } from '../ts/listProducts';
